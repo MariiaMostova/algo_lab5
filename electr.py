0bed5a236c3662bd3c5ff72a7ae107691a53d503
Mariia Mostova <were3437@gmail.com>
2019-12-04 12:48:22 +0200

    Renamed variables

--- electr.py
+++ electr.py
@@ -1,8 +1,8 @@
 import math
 
 
-def hypotenuse(h, w):
-    return math.sqrt(w ** 2 + h ** 2)
+def hypotenuse(height_array, width):
+    return math.sqrt(width ** 2 + height_array ** 2)
 
 
 def solution(previous, next):
@@ -14,14 +14,14 @@ def solution(previous, next):
 
 def get_the_worst(file):
     with open(file) as f:
-        w = int(f.readline())
+        width = int(f.readline())
         for line in f.readlines():
             heights = line.split(" ")
-            h = []
+            height_array = []
             for i in range(len(heights)):
-                h.append(int(heights[i]))
+                height_array.append(int(heights[i]))
     sum = 0
-    l = len(h)
+    l = len(height_array)
     for i in range(l - 1):
-        sum += hypotenuse(solution(h[i], h[i + 1]), w)
+        sum += hypotenuse(solution(height_array[i], height_array[i + 1]), width)
     return round(sum, 2)
